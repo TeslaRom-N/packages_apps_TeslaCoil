DirtyUnicorns/android_packages_apps_DU-Tweaks

Notification Count in Statusbar [2/2]
* Also fix buttons vector

Change-Id: I55f09dc8c7ac003a69f75cac502d01e1ad44190e

src/com/dirtyunicorns/dutweaks/fragments/StatusbarNotifications.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2015-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.dutweaks.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.PreferenceCategory;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+
+public class StatusbarNotifications extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+    private SwitchPreference mEnableNC;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar_notifications);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mEnableNC = (SwitchPreference) findPreference(STATUS_BAR_NOTIF_COUNT);
+        mEnableNC.setOnPreferenceChangeListener(this);
+        int EnableNC = Settings.System.getInt(getContentResolver(),
+                STATUS_BAR_NOTIF_COUNT, 0);
+        mEnableNC.setChecked(EnableNC != 0);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.DIRTYTWEAKS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if  (preference == mEnableNC) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), STATUS_BAR_NOTIF_COUNT,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

TeslaRom-N/packages_apps_TeslaCoil

src/com/gzr/teslacoil/fragments/StatusbarNotifications.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2015-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gzr.teslacoil.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.PreferenceCategory;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+
+public class StatusbarNotifications extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+    private SwitchPreference mEnableNC;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar_notifications);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mEnableNC = (SwitchPreference) findPreference(STATUS_BAR_NOTIF_COUNT);
+        mEnableNC.setOnPreferenceChangeListener(this);
+        int EnableNC = Settings.System.getInt(getContentResolver(),
+                STATUS_BAR_NOTIF_COUNT, 0);
+        mEnableNC.setChecked(EnableNC != 0);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.TESLACOIL;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if  (preference == mEnableNC) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), STATUS_BAR_NOTIF_COUNT,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}