DirtyUnicorns/android_packages_apps_DU-Tweaks

Remove/Disable OmniSwitch PS and features if package is uninstalled
Change-Id: I77849356720db6975a31a355a03aaa1d4dc87435

src/com/dirtyunicorns/dutweaks/fragments/Recents.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
+import com.android.internal.util.du.DuUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -151,6 +152,9 @@ public class Recents extends SettingsPreferenceFragment implements OnPreferenceC
         if (omniRecents) {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(false);
+        } else if (!DuUtils.isPackageInstalled(getActivity(), OMNISWITCH_PACKAGE_NAME)) {
+            mOmniRecents.setEnabled(false);
+            mStockRecents.setEnabled(true);
         } else {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(true);

src/com/dirtyunicorns/dutweaks/tabs/MultiTasking.java
@@ -21,22 +21,28 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.du.DuUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
 public class MultiTasking extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "MultiTasking";
+
+    private static final String KEY_OMNISWITCH = "omniswitch";
+    private static final String KEY_OMNI_SWITCH_PACKAGE_NAME = "org.omnirom.omniswitch";
+
+    private PreferenceScreen mOmniSwitch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,12 @@ public class MultiTasking extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.multitasking);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mOmniSwitch = (PreferenceScreen) findPreference(KEY_OMNISWITCH);
+        if (!DuUtils.isPackageInstalled(getActivity(), KEY_OMNI_SWITCH_PACKAGE_NAME)) {
+            prefSet.removePreference(mOmniSwitch);
+        }
     }
 
     @Override

TeslaRom-N/packages_apps_TeslaCoil

src/com/gzr/teslacoil/fragments/Recents.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
+import com.android.internal.util.tesla.TeslaUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -151,6 +152,9 @@ public class Recents extends SettingsPreferenceFragment implements OnPreferenceC
         if (omniRecents) {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(false);
+        } else if (!TeslaUtils.isPackageInstalled(getActivity(), OMNISWITCH_PACKAGE_NAME)) {
+            mOmniRecents.setEnabled(false);
+            mStockRecents.setEnabled(true);
         } else {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(true);

src/com/gzr/teslacoil/tabs/MultiTasking.java
@@ -21,22 +21,28 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.tesla.TeslaUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
 public class MultiTasking extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "MultiTasking";
+
+    private static final String KEY_OMNISWITCH = "omniswitch";
+    private static final String KEY_OMNI_SWITCH_PACKAGE_NAME = "org.omnirom.omniswitch";
+
+    private PreferenceScreen mOmniSwitch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,12 @@ public class MultiTasking extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.multitasking);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mOmniSwitch = (PreferenceScreen) findPreference(KEY_OMNISWITCH);
+        if (!TeslaUtils.isPackageInstalled(getActivity(), KEY_OMNI_SWITCH_PACKAGE_NAME)) {
+            prefSet.removePreference(mOmniSwitch);
+        }
     }
 
     @Override
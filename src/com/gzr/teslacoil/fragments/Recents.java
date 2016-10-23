443b4509c42b945bd5d72d31fc0a63368141fbeb
Alex Cruz <mazdarider23@gmail.com>
2016-10-23 06:48:27 +0200

    Remove/Disable OmniSwitch PS and features if package is uninstalled
    Change-Id: I77849356720db6975a31a355a03aaa1d4dc87435

--- src/com/gzr/teslacoil/fragments/Recents.java
+++ src/com/gzr/teslacoil/fragments/Recents.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
+import com.android.internal.util.tesla.TeslaUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -151,6 +152,9 @@ public class Recents extends SettingsPreferenceFragment implements OnPreferenceC
         if (omniRecents) {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(false);
+        } else if (!TeslaUtils.isPackageInstalled(getActivity(), OMNISWITCH_PACKAGE_NAME)) {
+            mOmniRecents.setEnabled(false);
+            mStockRecents.setEnabled(true);
         } else {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(true);
